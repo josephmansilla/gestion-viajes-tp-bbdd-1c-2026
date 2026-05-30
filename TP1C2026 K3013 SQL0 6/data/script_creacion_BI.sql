6102884c7f6c6a4154d371bd317dc2584a21bc23
Joseph Thomas Mansilla <jomansilla@frba.utn.edu.ar>
2026-05-30 15:53:51 -0300

    procedures para las views

--- TP1C2026 K3013 SQL0 6/data/script_creacion_BI.sql
+++ TP1C2026 K3013 SQL0 6/data/script_creacion_BI.sql
@@ -103,42 +103,105 @@ GO
 --1.Ticket promedio. Valor promedio de venta mensual según rango etario de
 --cliente y canal de venta.
 
+CREATE PROCEDURE sp_calcular_ticket_promedio (@rango_etario_cliente SMALLINT, @canal_venta SMALLINT) AS
+BEGIN
+	SELECT * FROM BI_canal_venta;
+END;
+GO
+
 CREATE VIEW ticket_promedio AS
-SELECT * FROM BI_canal_venta;
+	SELECT * FROM BI_canal_venta;
 GO
 
 --2. Distribución de Facturación: Porcentaje de facturación correspondiente a cada
 --tipo de servicio (ventas directas o propuestas a medida), para cada cuatrimestre
 --de cada año.
 
+CREATE PROCEDURE sp_calcular_distribucion_facturacion (@tipo_servicio SMALLINT, @rango_tiempo SMALLINT) AS
+BEGIN
+	SELECT * FROM BI_tipo_servicio;
+END;
+GO
+
+CREATE VIEW distribucion_facturacion AS
+		SELECT * FROM BI_tipo_servicio;
+GO
+
 --3. Ranking de solicitudes por temporadas: Cantidad de solicitudes realizadas,
 --agrupadas por temporada de cada año y rango etario del cliente.
+
+CREATE PROCEDURE sp_calcular_ranking_solicitudes_temporadas (@cantidad_solicitudes INT, @temporada SMALLINT, @rango_etario_cliente SMALLINT) AS
+BEGIN	
+	SELECT * FROM BI_temporadas;
+END;
+GO
+
 
 --4. Anticipación promedio de solicitudes. Promedio de días de anticipación con
 --los que el cliente realiza la solicitud de viaje, calculado como la diferencia entre
 --fecha de solicitud y fecha de inicio tentativa. Segmentado por rango etario del
 --cliente y cuatrimestre.
 
+CREATE PROCEDURE sp_calcular_anticipacion_promedio (@promedio_dias INT, @fecha_solicitud DATE, @fecha_inicio_tentativa DATE, @rango_etario_cliente SMALLINT, @rango_tiempo SMALLINT) AS
+BEGIN
+	SELECT * FROM BI_rangos_etario_cliente;
+END;
+GO
 --5. Tasa de aceptación de propuestas. Porcentaje de propuestas aceptadas sobre el
 --total de propuestas emitidas, calculado por cuatrimestre.
+
+CREATE PROCEDURE sp_calcular_tasa_aceptacion_propuestas (@tasa_aceptacion DECIMAL(5,2), @rango_tiempo SMALLINT) AS
+BEGIN
+	SELECT * FROM BI_estado_propuesta;
+END;
+GO
 
 --6. Cotización promedio por temporada: Importe promedio de las propuestas
 --emitidas, agrupado por temporada/año. Se deberá considerar como referencia
 --temporal la fecha de inicio del viaje.
+
+CREATE PROCEDURE sp_calcular_cotizacion_promedio_temporada (@temporada SMALLINT, @fecha_inicio_viaje DATE) AS
+BEGIN
+	SELECT * FROM BI_temporadas;
+END;
+GO
 
 --7. Tiempo promedio de respuesta: Tiempo promedio (en días) entre la fecha de
 --solicitud y la fecha de emisión de la propuesta. Segmentado por rango etario del
 --agente y mes. Se deberá considerar como referencia temporal la fecha de
 --solicitud.
 
+CREATE PROCEDURE sp_calcular_tiempo_respuesta (@promedio_dias INT, @fecha_solicitud DATE, @fecha_emision_propuesta DATE, @rango_etario_agente SMALLINT, @mes SMALLINT) AS
+BEGIN
+	SELECT * FROM BI_rangos_etario_agente;
+END;
+GO
+
 --8. Desvío de presupuesto: Desvío promedio entre el presupuesto estimado
 --informado en la solicitud y el importe de la propuesta generada.
+
+
+CREATE PROCEDURE sp_calcular_desvio_presupuesto (@presupuesto_estimado DECIMAL(10,2), @importe_propuesta DECIMAL(10,2)) AS
+BEGIN
+	SELECT * FROM BI_rangos_etario_agente;
+END;
+GO
 
 --9. Ranking de aspectos mejor y peor valorados: Promedio de puntaje por
 --aspecto evaluado, permitiendo identificar los aspectos con mejor y peor
 --valoración en cada cuatrimestre.
 
+
+CREATE PROCEDURE sp_calcular_ranking_aspectos (@aspecto_evaluado CHAR(30), @puntaje_promedio DECIMAL(5,2), @rango_tiempo SMALLINT) AS
+BEGIN 
+	SELECT * FROM BI_rangos_tiempo;
+END;
+GO
 --10. Satisfacción promedio por agente: Puntaje promedio obtenido en las
 --encuestas, segmentado por rango etario del agente y mes.
 
-
+CREATE PROCEDURE sp_calcular_satisfaccion_promedio_agente (@rango_etario_agente SMALLINT, @mes SMALLINT) AS
+BEGIN
+	SELECT * FROM BI_rangos_etario_agente;
+END;
+GO
